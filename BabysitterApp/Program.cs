1fe4183e60d904ce288fb79d1968b5cc57562c8b
Kelsey Butler <kelseybutler@KBUTLER-MBP-CHI.local>
2018-09-16 16:15:16 -0500

    Initializing a c# project

---
+++ BabysitterApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BabysitterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test for App");
+        }
+    }
+}
